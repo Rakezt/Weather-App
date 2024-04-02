d054cc19ff079b8d46f3ce5891de863cd7612cc5
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2024-04-02 17:58:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import WeatherApp from "./components/WeatherApp";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Weather App by Rakezt</h1>
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import "./WeatherApp.css";
+
+const WeatherApp = () => {
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Bengaluru");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=6127c54b73596dd413a74cbd642dc4c0`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setCity(data.main);
+    };
+
+    fetchApi();
+  }, [search]);
+  return (
+    <div className="container">
+      <div className="inputData">
+        <input
+          type="search"
+          className="inputField"
+          onChange={(event) => {
+            setSearch(event.target.value);
+          }}
+        />
+      </div>
+      {!city ? (
+        <p className="error">Incorrect city spelling</p>
+      ) : (
+        <div>
+          {" "}
+          <div className="info">
+            <h2 className="location">
+              <FontAwesomeIcon icon={faMapMarkerAlt} size="2x" /> {search}
+            </h2>
+            <h1 className="temp">{city.temp}°Celcius</h1>
+            <h3 className="tempDetails">Min: {city.temp_min}°Celcius </h3>
+            <h3 className="tempDetails"> Max :{city.temp_max}°Celcius</h3>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WeatherApp;
